2f2e4f26048380896d785e2123c10293cf192344
KronosEmporium <kronosemporium12@gmail.com>
2020-01-08 18:41:15 -0800

    Update tempconverter.php

--- tempconverter.php
+++ tempconverter.php
@@ -1,11 +1,51 @@
+<html>
+<head>
+<style>
+    .unitInfo {
+        position: absolute;
+        border: 1px solid black;
+        background: white;
+    }
+
+    #fInfo, #cInfo, #kInfo, #rInfo {
+        display: none;
+        width: 500px;
+        height: 500px;
+    }
+
+    iframe {
+        width: 100%;
+        height: 100%;
+    }
+
+    #fInfo:hover, #f:hover + #fInfo {
+        display: block;
+    }
+
+    #cInfo:hover, #c:hover + #cInfo {
+        display: block;
+    }
+
+    #kInfo:hover, #k:hover + #kInfo {
+        display: block;
+    }
+
+    #rInfo:hover, #r:hover + #rInfo {
+        display: block;
+    }
+</style>
+</head>
+<body>
 <?php
+
 // tempconverter.php
+
 if (isset($_POST["Temperature"])) {
-    
+
     $InputTemp = $_POST["Temperature"];
     $InputUnit = $_POST["FromTempUnit"];
     
-    // Shows data
+    // Do conversions
     if ($InputUnit === "Fahrenheit") {
         $ftemp = $InputTemp;
         $ctemp = convertToC($InputTemp);
@@ -27,30 +67,10 @@ if (isset($_POST["Temperature"])) {
         $ktemp = convertToC($InputTemp - 459.67) + 273.15;
         $rtemp = $InputTemp;
     }
-    
-    echo '
-<form action="" method="POST">
-    <p>Temperature to Convert</p>
-    <input type="number" name="Temperature" />
-    
-    <p>From:</p>
-    <input type="radio" name="FromTempUnit" value="Fahrenheit" checked> Fahrenheit
-    <input type="radio" name="FromTempUnit" value="Celsius"> Celsius
-    <input type="radio" name="FromTempUnit" value="Kelvin"> Kelvin
-    <input type="radio" name="FromTempUnit" value="Rankine"> Rankine
-    <input type="submit" value="Convert"/>
-    
-    <p>Input: ' . $InputTemp . ' ' . '&deg;' . $InputUnit . '
-    <p>Fahrenheit: ' . round($ftemp, 2) . '&deg;F</p>
-    <p>Celsius: ' . round($ctemp, 2) . '&deg;C</p>
-    <p>Kelvin: ' . round($ktemp, 2) . '&deg;K</p>
-    <p>Rankine: ' . round($rtemp, 2) . '&deg;Ra</p>
-</form>
-    ';
-    
-} else {
-    // Shows form
-    echo '
+}
+
+    // Show form
+echo '
 <form action="" method="POST">
     <p>Temperature Converter</p>
     <p>Enter Temperature: <input type="number" name="Temperature" />
@@ -61,18 +81,8 @@ if (isset($_POST["Temperature"])) {
     <input type="radio" name="FromTempUnit" value="Rankine"> Rankine
     <p></p>
     <input type="submit" value="Convert"/>
-    
-    <p>Fahrenheit:</p>
-    
-    <p>Celsius:</p>
-    
-    <p>Kelvin:</p>
-    
-    <p>Rankine:</p>
-    
 </form>
-    ';
-}
+';
 
 function convertToF($t) {
     return ($t * 9 / 5) + 32;
@@ -84,4 +94,44 @@ function convertToC($t) {
 
 ?>
 
+<br>
+<br>
+
+
+<p>Input: <?php if (isset($InputTemp) && isset($InputUnit)) echo $InputTemp . '&deg; ' . $InputUnit; ?></p>
+
+
+<a id="f">Fahrenheit: <?php if (isset($ftemp)) echo round($ftemp, 2); ?>&deg;F</a>
+<div class="unitInfo" id="fInfo"><iframe src="https://en.wikipedia.org/w/index.php?title=Fahrenheit&printable=yes"></iframe>
+</div>
+
+<br>
+<br>
+
+<a id="c">Celsius: <?php if (isset($ctemp)) echo round($ctemp, 2); ?>&deg;C</a>
+<div class="unitInfo" id="cInfo"><iframe src="https://en.wikipedia.org/w/index.php?title=Celsius&printable=yes"></iframe>
+</div>
+
+<br>
+<br>
+
+<a id="k">Kelvin: <?php if (isset($ktemp)) echo round($ktemp, 2); ?>&deg;K</a>
+<div class="unitInfo" id="kInfo"><iframe src="https://en.wikipedia.org/w/index.php?title=Kelvin&printable=yes"></iframe>
+</div>
+
+<br>
+<br>
+
+<a id="r">Rankine: <?php if (isset($rtemp)) echo round($rtemp, 2); ?>&deg;Ra</a>
+<div class="unitInfo" id="rInfo"><iframe src="https://en.wikipedia.org/w/index.php?title=Rankine_scale&printable=yes"></iframe>
+</div>
+
+<br>
+<br>
+<br>
+<br>
+
 <em>Note: Values are rounded to the nearest hundredth decimal place after conversion.</em>
+
+</body>
+</html>
